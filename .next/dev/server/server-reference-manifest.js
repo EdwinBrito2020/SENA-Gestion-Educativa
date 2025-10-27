23ddaac9e46ccf0f829231f64a58ca9947e82387
Edwin Olmedo Brito Arenas <edwinbrito@sena.edu.co>
2025-10-26 19:38:20 -0500

    Aplicar .gitignore y limpiar archivos ignorados

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JcutZkGAngl/y/r9ZkqmURM1UeTAt8c6oVYs/w8Tk/w=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_d3db8454._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_fa8dfc3d._.js"
-],
-    source: "dynamic"
-});
